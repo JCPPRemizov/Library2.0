bb742586c6b563b6e8977eedfbf13008437c278e
JPRemizov <isip_v.a.remizov@mpt.ru>
2023-05-30 18:43:16 +0300

    Some fix

--- Library2.0/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ Library2.0/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -109,7 +109,7 @@ namespace Library2._0 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Library2.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Library2.0;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
